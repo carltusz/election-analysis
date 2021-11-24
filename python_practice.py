90d8adbcfb14650532975b354b0679e602e8ce14
Carl Tusz <carltusz@gmail.com>
2021-11-23 19:43:39 -0500

    making sure it is saved

--- python_practice.py
+++ python_practice.py
@@ -1,1 +1,11 @@
-print("Hello World")
+counties = ["Arapahoe","Denver","Jefferson"]
+counties_dict = {"Arapahoe":422829, "Denver":463353, "Jefferson":432438}
+voting_data = [{"county":"Arapahoe","registered_voters":422829},
+                {"county":"Denver","registered_voters":463353},
+                {"county":"Jefferson","registered_voters":432438}]
+
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters:,} registered voters")
+
+for counties_dict in voting_data:
+    print(f"{counties_dict['county']} county has {counties_dict['registered_voters']:,} registered voters.")
